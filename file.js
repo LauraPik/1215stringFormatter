868b5c399b65c910e1a1efe1031188ebe4eec1f2
Laura <laura.pikele@stud.kitm.lt>
2023-12-15 23:18:06 +0200

    second commit

--- file.js
+++ file.js
@@ -1,14 +1,34 @@
 // Pateikę stringą, kurį sudaro tik raidės, parašykite funkciją, kuri gražina performatuotą stringą pagal pvz.
 
+// pasizymiu input
 let input = document.querySelector('input');
 
 
+// susikuriu funkcija, kuri konvertuoja ivesta input i norima formata
 const stringFormatter = str => {
-    const output = document.querySelector('.output');
-    output.textContent = str.value.split("").join("-");
+  let output = document.querySelector('.output');
+    
+  let word = '';
+  
+  for(let i=0; i<str.length; i++){
+    // su repeat pakartoju raides kiekvieno ciklo interacijos skaiciumi ir pridedu i nauja kintamaji word
+    word += str[i].repeat(i+1) 
+    // atskiriu zodzio raides bruksneliu 
+    if(i< str.length -1){
+      word += '-';
+    }
+  }
+
+  console.log(word)
+  // priskiriu gauta zodi i p 
+   output.innerHTML = word;
+
   };
 
+
+
+// pasizymiu input ir kvieciu susikurta funkcija, kuriai paduodu input parametra
 input.addEventListener('input', () => {
-    stringFormatter(input)
+    stringFormatter(input.value)
     
   });
